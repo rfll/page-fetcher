7027f539d62b859c0884c3b72faae4d7c5fa04be
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 04:51:12 +0000

    Added fs.writeFile as nested function

--- fetcher.js
+++ fetcher.js
@@ -11,6 +11,14 @@ console.log(path);
 
 const request = require('request');
 request(URL, (error, response, body) => {
+
+  // Write to file in command line arg 2
+  fs.writeFile(path, body, error => {
+    if (error) {
+      console.log('error:', error);
+    }
+  });
+
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
