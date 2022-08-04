9188fd677ff494d9e7b0ef9c5d1fde28389c13ee
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 04:38:35 +0000

    Access command line args with request

---
+++ fetcher.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// Access command line arg for URL
+const URL = process.argv[2];
+
+// Access command line arg for path
+const path = process.argv[3];
+
+console.log(URL);
+console.log(path);
+
+const request = require('request');
+request(URL, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
